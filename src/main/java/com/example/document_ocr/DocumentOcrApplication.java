4f14753dce066dbeec2145586676d8148d618a40
Andrew Huang <andrew03huang@gmail.com>
2024-06-11 11:32:48 -0400

    test

---
+++ src/main/java/com/example/document_ocr/DocumentOcrApplication.java
@@ -0,0 +1,13 @@
+package com.example.document_ocr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentOcrApplication {
+//TEST
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentOcrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/document_ocr/DocumentOcrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.document_ocr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentOcrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
